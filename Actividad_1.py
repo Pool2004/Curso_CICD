5a37c1ac9ee92e8ab6f7edd8ce5c255366a69aa9
Christian bolaños <cridi0712@gmail.com>
2025-09-04 02:40:02 -0500

    feat: solución actividad 1 de CI/CD

--- Actividad_1.py
+++ Actividad_1.py
@@ -31,9 +31,29 @@ Parte 1
 
 # PONER SOLUCIÓN PARTE 1
 
+# Variables tipo String
+nombre = "Christian"
+apellido = "Bolaños"
+print("Nombre:", nombre)
+print("Apellido:", apellido)
 
+# Variables tipo entero
+num1 = 10
+num2 = 5
+print("Número 1:", num1)
+print("Número 2:", num2)
 
+# Concatenación de strings
+concatenacion = nombre + " " + apellido
+print("Concatenación:", concatenacion)
 
+# Operaciones con enteros
+print("Suma:", num1 + num2)
+print("Resta:", num1 - num2)
+print("Multiplicación:", num1 * num2)
+print("División:", num1 / num2)
+print("División entera:", num1 // num2)
+print("Módulo:", num1 % num2)
 
 
 """
@@ -49,7 +69,27 @@ Parte 2
 
 """
 
-# PONER SOLUCIÓN PARTE 1
+# PONER SOLUCIÓN PARTE 2
+
+# Parte 2
+
+# Definir arreglo mixto (números y letras)
+mi_arreglo = [10, 20, "A", "B", 5]
+
+# Imprimir arreglo completo
+print("Arreglo completo:", mi_arreglo)
+
+# Primer elemento
+print("Primer elemento:", mi_arreglo[0])
+
+# Último elemento
+print("Último elemento:", mi_arreglo[-1])
+
+# Suma, resta, multiplicación entre primer y último (solo si son números)
+print("Suma primer y último:", mi_arreglo[0] + mi_arreglo[-1])
+print("Resta primer y último:", mi_arreglo[0] - mi_arreglo[-1])
+print("Multiplicación primer y último:", mi_arreglo[0] * mi_arreglo[-1])
+
 
 
 
@@ -72,6 +112,21 @@ Responda. ¿Qué herramientas en python para automatizar pruebas?
 R/
 
 """
+"""
+Parte 3
+
+R/ CI/CD significa Integración Continua (Continuous Integration) y Despliegue/Entrega Continua (Continuous Deployment/Delivery). 
+Es una práctica que automatiza la integración de código, pruebas y despliegues para garantizar calidad y rapidez en el desarrollo.
+
+R/ Herramientas para desplegar aplicaciones:
+- Docker, Kubernetes, Heroku, AWS, Azure, Google Cloud.
+
+R/ Herramientas para automatizar despliegues:
+- Jenkins, GitHub Actions, GitLab CI/CD, CircleCI, Travis CI.
+
+R/ Herramientas en Python para automatizar pruebas:
+- pytest, unittest, nose2, tox.
+"""
 
 
 """
@@ -92,4 +147,5 @@ Parte 4
 ENLACE A MI REPOSITORIO: 
 
 """
-
+# ENLACE A MI REPOSITORIO: 
+# https://github.com/ChristianBolaños/ci-cd-practicas
