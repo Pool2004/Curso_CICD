bbd2d535af115dcd6452ba3f71672122475e9ba2
samuel <samueltoro221@gmail.com>
2025-09-04 21:58:04 -0500

    actividad#1

--- SAMUEL.py
+++ SAMUEL.py
@@ -1,2 +1,53 @@
-a=1
-print(a)
+# Parte 1
+
+# Variables tipo String
+texto1 = "Hola"
+texto2 = "Mundo"
+print(texto1)
+print(texto2)
+
+# Variables tipo entero
+num1 = 10
+num2 = 3
+print(num1)
+print(num2)
+
+# Concatenación de Strings
+print(texto1 + " " + texto2)
+
+# Operaciones con enteros
+print("Suma:", num1 + num2)
+print("Resta:", num1 - num2)
+print("Multiplicación:", num1 * num2)
+print("División:", num1 / num2)
+print("División entera:", num1 // num2)
+print("Módulo:", num1 % num2)
+
+# Parte 2
+
+arreglo = [5, "a", 8, "b", 2]
+print(arreglo)
+print("Primer elemento:", arreglo[0])
+print("Último elemento:", arreglo[-1])
+
+# Solo operaciones entre números
+print("Suma primer y último elemento:", arreglo[0] + arreglo[-1])
+print("Resta primer y último elemento:", arreglo[0] - arreglo[-1])
+print("Multiplicación primer y último elemento:", arreglo[0] * arreglo[-1])
+
+# Parte 3
+
+# ¿Qué es CI/CD?
+# CI/CD significa Integración Continua y Entrega/Despliegue Continuo. Es una práctica de desarrollo de software donde los cambios se integran, prueban y despliegan automáticamente y de forma frecuente.
+
+# ¿Qué herramientas hay para desplegar aplicaciones?
+# Algunas herramientas son: Docker, Kubernetes, AWS Elastic Beanstalk, Heroku, Azure App Service.
+
+# ¿Qué herramientas hay para automatizar despliegues?
+# Jenkins, GitHub Actions, GitLab CI/CD, CircleCI, Travis CI, Azure DevOps.
+
+# ¿Qué herramientas en python para automatizar pruebas?
+# pytest, unittest, nose, behave, tox.
+
+#PARTE 4
+#mi repositorio https://github.com/samuel021893/despliegue.git
