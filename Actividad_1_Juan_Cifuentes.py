5f339bfb9627b7fac824bfeba4ca547536704186
Juan <juancifuentes4523@gmail.com>
2025-09-05 14:53:15 -0500

    Subiendo Actividad 1 - Juan Cifuentes

--- Actividad_1_Juan_Cifuentes.py
+++ Actividad_1_Juan_Cifuentes.py
@@ -31,7 +31,38 @@ Parte 1
 
 # PONER SOLUCIÓN PARTE 1
 
+# Variables tipo String
+print(" ")
 
+texto1 = "Hola"
+texto2 = "Mundo"
+print(texto1)
+print(texto2)
+
+print(" ")
+
+# Variables tipo entero
+num1 = 10
+num2 = 3
+print(num1)
+print(num2)
+
+print(" ")
+
+# Concatenación de Strings
+print(texto1 + " " + texto2)
+
+print(" ")
+
+# Operaciones con enteros
+print("Suma:", num1 + num2)
+print("Resta:", num1 - num2)
+print("Multiplicación:", num1 * num2)
+print("División:", num1 / num2)  # División normal (float)
+print("División entera:", num1 // num2)  # Cociente entero
+print("Módulo:", num1 % num2)  # Resto
+
+print(" ")
 
 
 
@@ -49,8 +80,25 @@ Parte 2
 
 """
 
-# PONER SOLUCIÓN PARTE 1
+# PONER SOLUCIÓN PARTE 2
 
+arreglo = [5, "A", 10, "B", 2]
+
+# Imprimir arreglo completo
+print(arreglo)
+
+# Primer elemento (posición 0)
+print("Primer elemento:", arreglo[0])
+
+# Último elemento (posición -1)
+print("Último elemento:", arreglo[-1])
+
+# Como la suma, resta y multiplicación solo tienen sentido con números,
+# tomamos el primer y último elemento que son números (5 y 2 en este caso)
+
+print("Suma:", arreglo[0] + arreglo[-1])
+print("Resta:", arreglo[0] - arreglo[-1])
+print("Multiplicación:", arreglo[0] * arreglo[-1])
 
 
 """
@@ -59,19 +107,29 @@ Parte 3
 Responda. ¿Qué es CI/CD?
 
 R/
+CI/CD significa Integración Continua (Continuous Integration) y Entrega/Despliegue Continuo (Continuous Delivery/Deployment).
+Es una práctica de desarrollo de software que busca automatizar la integración de código, pruebas y despliegues
+para entregar software de forma más rápida y confiable.
 
 Responda. ¿Qué herramientas hay para desplegar aplicaciones?
 
 R/
+Algunas herramientas son: Docker, Kubernetes, Heroku, AWS, Azure, Google Cloud, Netlify, Vercel.
 
 Responda. ¿Qué herramientas hay para automatizar despliegues?
 
 R/
+Jenkins, GitHub Actions, GitLab CI/CD, CircleCI, Travis CI, ArgoCD.
 
 Responda. ¿Qué herramientas en python para automatizar pruebas?
 R/
-
+Algunas son: unittest, pytest, nose2, doctest, behave (para pruebas BDD).
 """
+
+
+
+
+
 
 
 """
@@ -89,7 +147,6 @@ Parte 4
 • El repositorio debe tener su respectivo README
 • Debe adjuntar el enlace en este documento
 
-ENLACE A MI REPOSITORIO: 
 
 """
-
+# ENLACE A MI REPOSITORIO: https://github.com/juancrakk45/juan_Cifuentes.git
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Curso_CICD
 Curso sobre CI/CD - CIADET | Estudiantes.
+hola
+# https://github.com/juancrakk45/juan_Cifuentes.git
