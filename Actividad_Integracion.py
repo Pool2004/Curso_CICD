b15dc5c6214a6f9e2fd55fd81b7af6b8fa5df631
JGomz0021 <giraldojulian767@gmail.com>
2025-09-13 11:10:57 -0500

    feat: promedio y condicionales

--- Actividad_Integracion.py
+++ Actividad_Integracion.py
@@ -1,18 +1,33 @@
 # Grupo de Cristian - Variables iniciales y input de cantidad de estudiantes
 
 
+# grupo de christian - mostrar  Variables iniciales y input de cantidad de estudiantes
 
+cantidad_estudiantes = int(input("Ingrese la cantidad de estudiantes: "))
+nombres = []
+notas = []
 
 
 # Grupo de Jhon - Ciclo while preguntando nombre y nota y agregando a los arreglos
-
+i = 0  
+while i < cantidad_estudiantes:
+    nombre = input(f"Ingrese el nombre del estudiante {i+1}: ")
+    nota = float(input(f"Ingrese la nota del estudiante {i+1}: "))
+    nombres.append(nombre)
+    notas.append(nota)
+    i += 1
 
 
 
 
 # Grupo de Juan - Promedio general de notas y condicionales
 
+promedio = sum(notas) / len(notas)
 
+if (promedio >= 3.0):
+    print("El curso esta aprobado")
+else:
+    print("El curso esta reprobado")
 
 
 
